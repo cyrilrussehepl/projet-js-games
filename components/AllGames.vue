80192b0012c21b00b0ef93ac8eae8e99be7f5cc4
Cyril Russe <cyril.russe@student.hepl.be>
2023-05-31 11:25:00 +0200

    combobox filter in allgames page

--- components/AllGames.vue
+++ components/AllGames.vue
@@ -1,78 +1,87 @@
 <template>
-  <!-- All Games Start -->
-  <div class="container mb-120">
-    <div class="filter-wrap bg-secondary-100 rounded-2xl px-5 py-7 mb-10">
-      <div class="flex flex-col sm:flex-row justify-between">
-        <div class="category-filter">
-          <div class="filter-option">
-            <select
-              id="selectgame"
-              class="px-5 h-14 w-full mb-8 sm:mb-0 sm:w-64 text-white bg-secondary-100 border-2 border-solid rounded-lg focus:outline-none border-gray-400 block"
-            >
-              <option value="" class="bg-black">All Category</option>
-              <option value="1">Action</option>
-            </select>
-          </div>
-        </div>
-        <div class="search-filter">
-          <div class="search-bx">
-            <form action="#" class="relative">
-              <input
-                type="text"
-                placeholder="Search"
-                class="px-5 h-14 w-full sm:w-64 bg-secondary-100 border-2 border-solid rounded-lg focus:outline-none"
-              />
-              <button
-                type="submit"
-                class="absolute px-5 top-0 right-0 bg-transparent transition-all inline-block h-full text-white hover:text-primary"
-              >
-                <i class="icofont-search-1"></i>
-              </button>
-            </form>
-          </div>
-        </div>
+   <!-- All Games Start -->
+   <div class="container mb-120">
+      <div class="filter-wrap bg-secondary-100 rounded-2xl px-5 py-7 mb-10">
+         <div class="flex flex-col sm:flex-row justify-between">
+            <div class="category-filter">
+               <div class="filter-option">
+                  <select
+                     id="selectgame"
+                     class="px-5 h-14 w-full mb-8 sm:mb-0 sm:w-64 text-white bg-secondary-100 border-2 border-solid rounded-lg focus:outline-none border-gray-400 block"
+                     v-model="valueCombobox"
+                  >
+                     <option value="0" class="bg-black" selected>
+                        All Category
+                     </option>
+                     <option value="1">Platform</option>
+                     <option value="2">Puzzle</option>
+                     <option value="3">Statégie</option>
+                  </select>
+               </div>
+            </div>
+            <div class="search-filter">
+               <div class="search-bx">
+                  <!-- <div class="relative">
+                     <input
+                        type="text"
+                        placeholder="Search"
+                        class="px-5 h-14 w-full sm:w-64 bg-secondary-100 border-2 border-solid rounded-lg focus:outline-none"
+                        v-model="searchValue"
+                        @input="handleInputChange"
+                     />
+                     <button
+                        class="absolute px-5 top-0 right-0 bg-transparent transition-all inline-block h-full text-white hover:text-primary"
+                     >
+                        <i class="icofont-search-1"></i>
+                     </button>
+                  </div> -->
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
 
-    <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 gap-6 item-display"
-    >
       <div
-        class="item relative group before:content-[''] before:absolute before:w-full before:h-full before:opacity-0 before:bg-black before:top-0 before:left-0 before:right-0 before:bottom-0 overflow-hidden hover:before:opacity-70 transition-all hover:before:transition-all before:border-4 before:border-white before:rounded-4xl before:border-opacity-20"
-        v-for="(games, index) in gameHome"
-        :key="index"
+         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 gap-6 item-display"
       >
-        <img
-          class="w-full rounded-4xl"
-          :src="games.gameImage"
-          alt="Feature Icon"
-        />
-        <n-link
-          :to="`/game/${games.slug}`"
-          class="group primary-btn absolute-center opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all"
-          style="
-            background-image: url(/images/others/btn-bg_contours-roses.webp);
-          "
-        >
-          {{ games.btnName }}
-          <img
-            src="/images/icon/arrrow-icon.webp"
-            alt="Arrow Icon"
-            class="ml-3 w-5 h-5 group-hover:ml-4 transition-all"
-          />
-        </n-link>
+         <div
+            class="item relative group before:content-[''] before:absolute before:w-full before:h-full before:opacity-0 before:bg-black before:top-0 before:left-0 before:right-0 before:bottom-0 overflow-hidden hover:before:opacity-70 transition-all hover:before:transition-all before:border-4 before:border-white before:rounded-4xl before:border-opacity-20"
+            v-for="(games, index) in gameHome"
+            :key="index"
+            v-if="games.category == valueCombobox || valueCombobox == 0"
+         >
+            <img
+               class="w-full rounded-4xl"
+               :src="games.gameImage"
+               alt="Feature Icon"
+            />
+            <n-link
+               :to="`/game/${games.slug}`"
+               class="group primary-btn absolute-center opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all"
+               style="
+                  background-image: url(/images/others/btn-bg_contours-roses.webp);
+               "
+            >
+               {{ games.btnName }}
+               <img
+                  src="/images/icon/arrrow-icon.webp"
+                  alt="Arrow Icon"
+                  class="ml-3 w-5 h-5 group-hover:ml-4 transition-all"
+               />
+            </n-link>
+         </div>
       </div>
-    </div>
-  </div>
-  <!-- All Games End -->
+   </div>
+   <!-- All Games End -->
 </template>
 <script>
-  import gameHome from "@/data/games.json";
-  export default {
-    data() {
+import gameHome from '@/data/games.json';
+export default {
+   data() {
+      var valueCombobox = '0';
       return {
-        gameHome,
+         gameHome,
+         valueCombobox,
       };
-    },
-  };
+   },
+};
 </script>
