0d2b18d455cff92d416018584e63e1ef9e4816a2
Cyril Russe <cyril.russe@student.hepl.be>
2023-05-31 11:28:30 +0200

    warning fixed

--- components/Games/GameDetails.vue
+++ components/Games/GameDetails.vue
@@ -15,7 +15,10 @@
 
          <div class="gameslide my-15">
             <swiper class="swiper" :options="swiperOption">
-               <swiper-slide v-for="gameImg in games.presentationImg">
+               <swiper-slide
+                  v-for="gameImg in games.presentationImg"
+                  :key="gameImg"
+               >
                   <div class="relative">
                      <img
                         class="sm:h-full h-64 w-full object-cover"
